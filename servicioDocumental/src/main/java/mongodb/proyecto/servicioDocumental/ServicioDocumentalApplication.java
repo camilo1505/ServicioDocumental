3895335d816859af4d8c9ff85a9f98bcb622418c
junpaflorez <junpaflorez@gmail.com>
2018-12-27 15:41:07 -0500

    spring initializer

---
+++ servicioDocumental/src/main/java/mongodb/proyecto/servicioDocumental/ServicioDocumentalApplication.java
@@ -0,0 +1,14 @@
+package mongodb.proyecto.servicioDocumental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioDocumentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioDocumentalApplication.class, args);
+	}
+
+}
+
