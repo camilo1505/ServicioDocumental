d8496ec162f3404e529b51f907fb904caa65ca41
junpaflorez <junpaflorez@gmail.com>
2018-12-27 15:57:09 -0500

    proyecto

---
+++ ServicioDocumentalSpringboot/src/main/java/mongo/proyect/servicioDocumental/ServicioDocumentalApplication.java
@@ -0,0 +1,14 @@
+package mongo.proyect.servicioDocumental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioDocumentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioDocumentalApplication.class, args);
+	}
+
+}
+
