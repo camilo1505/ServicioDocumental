0f522300dbe723b9e375420caa507bde4b6913a5
junpaflorez <junpaflorez@gmail.com>
2019-02-04 11:04:57 -0500

    request header

--- src/main/java/mongo/proyect/servicioDocumental/controller/DocumentoController.java
+++ src/main/java/mongo/proyect/servicioDocumental/controller/DocumentoController.java
@@ -14,11 +14,11 @@ import mongo.proyect.servicioDocumental.service.DocumentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -83,7 +83,7 @@ public class DocumentoController {
     public ResponseEntity<?> guardarArchivo(
             @RequestParam("nombreDocumento") String nombreDocumento,
             @RequestParam("autor")String autor,
-            @RequestBody MultipartFile file,
+            @RequestHeader MultipartFile file,
             @RequestParam("ocr") boolean ocr) throws Exception{
         
         DocumentoDTO documento = new DocumentoDTO();
