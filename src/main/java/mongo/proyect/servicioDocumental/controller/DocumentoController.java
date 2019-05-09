45ad02e76e31ffc2f39320ce5e583d9f8f29c5fc
Juan Pablo Florez Caicedo <junpaflorez@gmail.com>
2019-05-09 00:41:29 -0500

    ajustes

--- src/main/java/mongo/proyect/servicioDocumental/controller/DocumentoController.java
+++ src/main/java/mongo/proyect/servicioDocumental/controller/DocumentoController.java
@@ -31,7 +31,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @author junpa
  */
 
-@CrossOrigin(origins = "http://localhost:8081")
+//@CrossOrigin(origins = "http://localhost:8081")
 @RestController
 @RequestMapping("/documento")
 public class DocumentoController {
